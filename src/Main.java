824ecf46bb9c740c46be03af37b66cddc3edc6f2
Светлана Тумоякова <Sveta.Tumoyakova@yandex.ru>
2026-01-17 22:45:49 +0700

    update project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import people.Graduate;
+import people.Student;
+import people.Teacher;
+
 public class Main {
     public static void main(String[] args) {
-
+        Student student1 = new Student("Капустин", "Роман", 18, "КБ", "КБ-31", 5);
+        System.out.print(student1);
+        Student student2 = new Student("Звездочкин", "Борис", 32, "РПО", "ITЗ-21", 5);
+        System.out.print(student2);
+        Teacher teacher1 = new Teacher("Ягодкин", "Матвей", 38, "Data Science и аналитика", 116);
+        System.out.print(teacher1);
+        Graduate graduate1 = new Graduate("Пуговкин", "Сергей", 19, "РПО", "IT-41", 5, "Многофакторная аутентификация с биометрией и аппаратными токенами");
+        System.out.print(graduate1);
+        System.out.println();
     }
 }
